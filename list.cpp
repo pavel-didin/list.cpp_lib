736f79ce90695213fdd439a0233e98f992fd8207
pavel-didin <65916221+pavel-didin@users.noreply.github.com>
2020-05-26 19:53:20 +0300

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -4,28 +4,6 @@
 using namespace std;
 
 template<typename T>
-class List
-{
-    private:
-    struct node;
-    node* head, *tail;
-    size_t count;
-    public:
-    List();
-    ~List();
-    void addToHead(const T&);
-    void addToTail(const T&);
-    void showFromHead();
-    void showFromTail();
-    int search(const T&) const;
-    void eraseElementByVal(const T&);
-    void insert(const size_t&, const T&);
-    void sort(const bool& ascending = true);
-
-};
-
-
-template<typename T>
 struct List<T>::node
 {
     T data;
